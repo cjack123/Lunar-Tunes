c05431a6f1f7a63f62ce858ace01fc8bf119dbc9
Chaneice Jackson <c.jackson0126@gmail.com>
2022-12-24 23:37:35 -0600

    commitment

---
+++ src/components/Auth/AuthManager.js
@@ -0,0 +1,1 @@
+const auth_URL = "https://api.spotify.com/authorize?client_id=8b945ef10ea24755b83ac50cede405a0&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
